11a108a80520bff7aef33e4512bbfcf397567fe2
Frank Mugendi <njaombefrank@gmail.com>
2025-01-29 19:19:55 +0300

    cors

--- Server/server.js
+++ Server/server.js
@@ -11,7 +11,7 @@ const app = express();
 
 const corsOptions = {
   // origin: "https://ev-locator-six.vercel.app",
-  origin: "https://ev-locator-six.vercel.app/",
+  origin: "https://ev-locator-six.vercel.app",
   credentials: true,
 };
 app.use(cors(corsOptions));
